bc93daa2308c1625122d858f9a6d43685ed50a63
sollarp <szpitor@gmail.com>
2022-01-13 22:01:29 +0000

    Added QR code generator

--- app/src/main/java/com/example/wificardscanner/MainActivity.kt
+++ app/src/main/java/com/example/wificardscanner/MainActivity.kt
@@ -5,6 +5,7 @@ import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
@@ -13,16 +14,23 @@ import android.widget.*
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.google.android.gms.tasks.Task
+import com.google.android.material.textfield.TextInputLayout
 import com.google.mlkit.vision.text.TextRecognition
 import com.google.mlkit.vision.text.latin.TextRecognizerOptions
 import kotlinx.android.synthetic.main.activity_main.*
 import com.google.mlkit.vision.common.InputImage
 import com.google.mlkit.vision.text.Text
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.EncodeHintType
+import com.google.zxing.qrcode.QRCodeWriter
 
 class MainActivity : AppCompatActivity() {
 
     private var allItemListArray = ArrayList<ItemContainer>()
     private lateinit var result: Task<Text>
+    private var ssid = ""
+    private var password = ""
+    private var preSharedKey = "WPA2"
 
     companion object {
         private const val CAMERA_PERMISSON_CODE = 1
@@ -52,8 +60,20 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this@MainActivity, "No Image was selected", Toast.LENGTH_SHORT).show()
         }
         submitButton.setOnClickListener {
+            generateQR()
             Toast.makeText(this@MainActivity, "No Image was selected", Toast.LENGTH_SHORT).show()
         }
+        val keyLists: Array<String> = resources.getStringArray(R.array.shared_keys)
+        val listAdapter: ArrayAdapter<String> =
+            ArrayAdapter(this, R.layout.dropdown_item, keyLists)
+        val autocompleteTV3 = findViewById<AutoCompleteTextView>(R.id.autoCompleteTextView3)
+        autocompleteTV3.setAdapter(listAdapter)
+        autocompleteTV3.onItemClickListener =
+            AdapterView.OnItemClickListener { parent, view, position, id ->
+                val selectedItemText3 = parent.getItemAtPosition(position)
+                preSharedKey = selectedItemText3.toString()
+            }
+
     }
 
     override fun onRequestPermissionsResult(
@@ -121,7 +141,6 @@ class MainActivity : AppCompatActivity() {
             for (inLine in inBlock.lines) {
                 for (element in inLine.elements) {
                     listItem = append(listItem, element.text)
-                    //println(listItem.contentToString())
                 }
             }
         }
@@ -136,7 +155,7 @@ class MainActivity : AppCompatActivity() {
             AdapterView.OnItemClickListener { parent, view, position, id ->
                 val selectedItemText = parent.getItemAtPosition(position)
                 testView.text = "selected: "
-                Toast.makeText(this@MainActivity, selectedItemText.toString(), Toast.LENGTH_SHORT).show()
+                //Toast.makeText(this@MainActivity, selectedItemText.toString(), Toast.LENGTH_SHORT).show()
 
             }
         val arrayAdapter2: ArrayAdapter<String> = ArrayAdapter(this, R.layout.dropdown_item, listItem)
@@ -146,9 +165,27 @@ class MainActivity : AppCompatActivity() {
         autocompleteTV2.onItemClickListener =
             AdapterView.OnItemClickListener { parent, view, position, id ->
                 val selectedItemText2 = parent.getItemAtPosition(position)
-                Toast.makeText(this@MainActivity, selectedItemText2.toString(), Toast.LENGTH_SHORT).show()
 
             }
+    }
+    private fun generateQR () {
+
+        val textQR = "WIFI:T:${preSharedKey};S:${ssid};P:${password};;"
+
+        val size = 512 //pixels
+        //val qrCodeContent = "WIFI:S:$ssid;T:WPA;P:$password;;"
+
+        val hints = hashMapOf<EncodeHintType, Int>().also { it[EncodeHintType.MARGIN] = 1 } // Make the QR code buffer border narrower
+        val bits = QRCodeWriter().encode(textQR, BarcodeFormat.QR_CODE, size, size)
+        val qrImage = Bitmap.createBitmap(size, size, Bitmap.Config.RGB_565).also {
+            for (x in 0 until size) {
+                for (y in 0 until size) {
+                    it.setPixel(x, y, if (bits[x, y]) Color.BLACK else Color.WHITE)
+                }
+            }
+        }
+        imageView.setImageBitmap(qrImage)
+
     }
 }
 
